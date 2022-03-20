65a8a4feab4c0acbbe0180e17c062cf0c2a6a70a
DarkAssassin23 <15916504+DarkAssassin23@users.noreply.github.com>
2022-03-19 21:59:33 -0400

    added script to run multiple connections at once

--- basicClient.py
+++ basicClient.py
@@ -1,11 +1,42 @@
 #!/usr/bin/python3
 
-import socket
+import socket, ipaddress, sys
 
-# Host and port to connect to
+# Default host and port to connect to
 HOST = "127.0.0.1"
 PORT = 8989
 
+
+# Checks if IP Address passed in is valid
+def validIP(ip):
+	bits = ip.split('.')
+	if(len(bits)>4 or len(bits)<4):
+		return False
+	for x in bits:
+		if(x.isdigit()):
+			if(int(x)>255):
+				return False
+			elif(not x.isdigit()):
+				return False
+	return True
+
+# Checks if the Port number passed in is Valid
+def validPort(port):
+	if(not port.isdigit()):
+		return False
+	if(int(port)<0 or int(port)>65535):
+		return False
+	return True
+
+if(len(sys.argv)>2):
+	if(validIP(sys.argv[1])):
+		HOST = sys.argv[1]
+	if(validPort(sys.argv[2])):
+		PORT = (int)(sys.argv[2])
+elif(len(sys.argv)>1):
+	if(validIP(sys.argv[1])):
+		HOST = sys.argv[1]
+	
 # Create socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
